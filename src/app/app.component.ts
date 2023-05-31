9525d50698185dc468c524c47e2e4fca672be76b
Maria <maria123321borovikova@gmail.com>
2023-05-31 20:33:24 +0200

    main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { StoreService } from './store.service';
+import Product from './types/product.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Nike';
+  products$: Observable<Product[]>;
+  cartItems$: Observable<Product[]>;
+  total$: Observable<number>;
+
+  constructor(private storeService: StoreService) {
+    this.products$ = this.storeService.products$;
+    this.cartItems$ = this.storeService.cartItems$;
+    this.total$ = this.storeService.total$;
+  }
+
+  addItemsToCart(product: Product): void {
+    this.storeService.addItemsToCart(product);
+  }
+
+  removeItemFromCartByIndex(index: number): void {
+    this.storeService.removeItemFromCartByIndex(index);
+  }
 }
---
+++ src/app/store.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { ajax } from 'rxjs/ajax';
+import Product from './types/product.model';
+import { BehaviorSubject, combineLatest, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StoreService {
+  private cartItemsSubject = new BehaviorSubject<Product[]>([]);
+  cartItems$ = this.cartItemsSubject.asObservable();
+
+  products$ = ajax.getJSON<Product[]>('/assets/store.json');
+
+
+  total$ = this.cartItems$.pipe(
+    map((items: Product[]) => {
+      return items.reduce((acc, item) => acc + item.price, 0);
+    })
+  );
+
+  addItemsToCart(product: Product) {
+    const currentItems = this.cartItemsSubject.getValue();
+    const newItems = [...currentItems, product];
+    this.cartItemsSubject.next(newItems);
+  }
+
+  removeItemFromCartByIndex(index: number) {
+    const currentItems = this.cartItemsSubject.getValue();
+    const newItems = currentItems.filter((item, i) => i !== index);
+    this.cartItemsSubject.next(newItems);
+  }
+
+  constructor() { }
+}
