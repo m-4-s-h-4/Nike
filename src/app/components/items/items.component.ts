a8c4cae149d083309466ca718ee7dff518414ad3
Maria <maria123321borovikova@gmail.com>
2023-06-08 12:25:38 +0200

    main

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,28 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import Product from '../../types/product.model';
 import { CartService } from 'src/app/services/cart/cart.service';
 import { StoreService } from 'src/app/services/store/store.service';
-
+import { FilterPipe } from '../../pipe/filter.pipe';
 
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
-  styleUrls: ['./items.component.css']
+  styleUrls: ['./items.component.css'],
+  providers: [FilterPipe]
 })
-export class ItemsComponent {
+export class ItemsComponent implements OnInit {
+  currentPage$ = this.storeService.currentPage$;
+  totalPages$ = this.storeService.totalPages$;
   products$: Observable<Product[]>;
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
   searchKey: string = "";
 
   constructor(
-    private storeService: StoreService,
-    private cartService: CartService
+    public storeService: StoreService,
+    private cartService: CartService,
+    private filterPipe: FilterPipe
   ) {
     this.products$ = this.storeService.filteredProducts$;
 
     this.storeService.search.subscribe((val: any) => {
       this.searchKey = val;
+      this.filterProducts();
     });
+  }
+
+  ngOnInit(): void {
+    this.products$.subscribe(products => {
+      this.products = products;
+      this.filterProducts();
+    });
+  }
+
+  filterProducts(): void {
+    this.filteredProducts = this.filterPipe.transform(this.products, this.searchKey, 'title');
   }
 
   highlightTitle(title: string): string {
